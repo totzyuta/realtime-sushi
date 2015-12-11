b99c654dc0763c0d68db05ca56128bdbe9f2415a
totzYuta <yuta.totz@gmail.com>
2015-12-12 04:31:31 +0900

    define Messages#create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,12 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+    ActionCable.server.broadcast "messages",
+      message: params[:message][:body],
+      username: params[:messages][:username]
+
+    head :ok
+  end
 end
